14b842eaf39e94dcc3160651b8ef3be36f291fa9
MobtgZhang <3215497340@qq.com>
2022-09-26 15:08:51 +0800

    Add files via upload

--- znlp/layers/__init__.py
+++ znlp/layers/__init__.py
@@ -2,6 +2,6 @@ from .att import MutiAttentionLayer
 from .out import MatchNetwork,ReadoutLayer
 from .caps import CapsuleLayer
 from .cnn import MultiCNNLayer
-from .graphs import GraphConvolution,GraphGNN,VGAE
+from .graphs import GraphConvolution,GraphGNN,VGAE,HyperGraphAttentionLayerSparse
 from .sfu import SFU
 
