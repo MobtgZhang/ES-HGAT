e427ab4b4bafd93d73eda0fec68975b88651d4ed
mobtgzhang <mobtgzhang@outlook.com>
2023-01-23 10:17:58 +0800

    update the codes

--- csv_process.py
+++ csv_process.py
@@ -12,50 +12,6 @@ from bs4 import BeautifulSoup
 logger = logging.getLogger()
 from znlp.utils import split_content_labels
 
-def split_aichallenger2018_dataset(data_dir,result_dir,tokenizer,train_per=0.75):
-    train_file_name = os.path.join(data_dir,"sentiment_analysis_trainingset.csv")
-    dev_file_name = os.path.join(data_dir,"sentiment_analysis_validationset.csv")
-    id2label = ['location_traffic_convenience',
-      'location_distance_from_business_district', 'location_easy_to_find',
-      'service_wait_time', 'service_waiters_attitude',
-      'service_parking_convenience', 'service_serving_speed', 'price_level',
-      'price_cost_effective', 'price_discount', 'environment_decoration',
-      'environment_noise', 'environment_space', 'environment_cleaness',
-      'dish_portion', 'dish_taste', 'dish_look', 'dish_recommendation',
-      'others_overall_experience', 'others_willing_to_consume_again']
-    label2id = {word:idx for idx,word in enumerate(id2label)}
-    save_data_dict = {
-      "id2label":id2label,
-      "label2id":label2id
-    }
-    save_dict_file = os.path.join(result_dir,"names-dict.json")
-    with open(save_dict_file,mode="w",encoding="utf-8") as wfp:
-        jline = json.dumps(save_data_dict)
-        wfp.write(jline)
-    dev_per = (1.0-train_per)/2
-    test_per = dev_per
-    train_dataset = pd.read_csv(train_file_name)
-    dev_dataset = pd.read_csv(dev_file_name)
-    all_dataset = pd.concat([train_dataset,dev_dataset])
-    del train_dataset,dev_dataset
-    all_dataset = sklearn.utils.shuffle(all_dataset)
-    all_dataset[id2label] = all_dataset[id2label]+2
-    
-    train_len = int(train_per*len(all_dataset))
-    dev_len = train_len + int(dev_per*len(all_dataset))
-    
-    train_dataset = all_dataset[:train_len]
-    valid_dataset = all_dataset[train_len:dev_len]
-    test_dataset = all_dataset[dev_len:]
-    dataset_list = [train_dataset,valid_dataset,test_dataset]
-    names_list = ['train','valid','test']
-    for name,dataset in zip(names_list,dataset_list):
-        id_dataset = dataset['id'].values.tolist()
-        content_dataset = dataset['content'].values.tolist()
-        labels_dataset = dataset[id2label].values.tolist()
-        save_dataset_file = os.path.join(result_dir,"%sset.json"%name)
-        split_content_labels(save_dataset_file,tokenizer,id_dataset,content_dataset,labels_dataset)
-        logger.info("File saved in %s"%save_dataset_file)
 def split_clueemotion2020_dataset(data_dir,result_dir,tokenizer):
     data_type_list = ['train','valid','test']
     id2label = ['like','happiness','sadness','anger','disgust','fear','surprise']
@@ -82,76 +38,6 @@ def split_clueemotion2020_dataset(data_dir,result_dir,tokenizer):
                     }
                     jline = json.dumps(save_data_dict)
                     wfp.write(jline+"\n")
-def split_wrime_dataset(data_dir,result_dir,tokenizer,version,name="Writer"):
-    load_file_name = os.path.join(data_dir,version+".tsv")
-    dataset = pd.read_csv(load_file_name,sep='\t')
-    
-    train_dataset = dataset[dataset['Train/Dev/Test']=='train'].drop(labels=['Train/Dev/Test'],axis=1).copy()
-    valid_dataset = dataset[dataset['Train/Dev/Test']=='dev'].drop(labels=['Train/Dev/Test'],axis=1).copy()
-    test_dataset = dataset[dataset['Train/Dev/Test']=='test'].drop(labels=['Train/Dev/Test'],axis=1).copy()
-    del dataset
-    data_list = [train_dataset,valid_dataset,test_dataset]
-    names_list = ['train','valid','test']
-    id2label = ['_Joy','_Sadness', '_Anticipation', '_Surprise','_Anger', '_Fear', '_Disgust', '_Trust']
-    id2label = ["%s"%name+item for item in id2label]
-    label2id = {word:idx for idx,word in enumerate(id2label)}
-    save_data_dict = {
-      "id2label":id2label,
-      "label2id":label2id
-    }
-    save_dict_file = os.path.join(result_dir,"names-dict.json")
-    with open(save_dict_file,mode="w",encoding="utf-8") as wfp:
-        jline = json.dumps(save_data_dict)
-        wfp.write(jline)
-        
-   
-    for name,dataset in zip(names_list,data_list):
-        id_dataset = dataset['UserID'].values.tolist()
-        content_dataset = dataset['Sentence'].values.tolist()
-        labels_dataset = dataset[id2label].values.tolist()
-        save_dataset_file = os.path.join(result_dir,"%sset.json"%name)
-        split_content_labels(save_dataset_file,tokenizer,id_dataset,content_dataset,labels_dataset)
-        logger.info("File saved in %s"%save_dataset_file)
-def split_carsreviews_dataset(data_dir,result_dir,tokenizer,train_per=0.7):
-    load_file_name = os.path.join(data_dir,"train.csv")
-    all_dataset = pd.read_csv(load_file_name)
-    id2label = list(set(all_dataset["subject"]))
-    label2id = {word:idx for idx,word in enumerate(id2label)}
-    save_data_dict = {
-        "id2label":id2label,
-        "label2id":label2id
-    }
-    save_dict_file = os.path.join(result_dir,"names-dict.json")
-    with open(save_dict_file,mode="w",encoding="utf-8") as wfp:
-        jline = json.dumps(save_data_dict)
-        wfp.write(jline)
-    all_dataset = sklearn.utils.shuffle(all_dataset)
-    length = len(all_dataset)
-    categories = len(label2id)
-    val_mat = np.ones(shape=(length,categories),dtype=np.int64)*(-2)
-    for k in range(length):
-        val_mat[k,label2id[all_dataset.loc[k,"subject"]]] = all_dataset.loc[k,"sentiment_value"]
-    val_mat = val_mat + 2
-    all_dataset[id2label] = val_mat
-    all_dataset.drop(columns = ["subject","sentiment_value","sentiment_word"],inplace = True)
-    dev_per = (1.0-train_per)/2.0
-    train_len = int(train_per*len(all_dataset))
-    dev_len = int((train_per+dev_per)*len(all_dataset))
-    train_dataset = all_dataset.iloc[:train_len].copy()
-    dev_dataset = all_dataset.iloc[train_len:dev_len].copy()
-    test_dataset = all_dataset.iloc[dev_len:].copy()
-    del all_dataset
-    dataset_list = [train_dataset,dev_dataset,test_dataset]
-    names_list = ['train','valid','test']
-    
-    for name,dataset in zip(names_list,dataset_list):
-        id_dataset = dataset['content_id'].values.tolist()
-        content_dataset = dataset['content'].values.tolist()
-        labels_dataset = dataset[id2label].values.tolist()
-        save_dataset_file = os.path.join(result_dir,"%sset.json"%name)
-        split_content_labels(save_dataset_file,tokenizer,id_dataset,content_dataset,labels_dataset)
-        logger.info("File saved in %s"%save_dataset_file)
-
 def split_simplifyweibo4moods_dataset(data_dir,result_dir,tokenizer,train_per = 0.7):
     load_train_file = os.path.join(data_dir,'simplifyweibo_4_moods.csv')
     load_labels_file = os.path.join(data_dir,'labels.txt')
