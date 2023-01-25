ae8531a78f8ea5436ecf8670dd068a0c6b79f00d
mobtgzhang <mobtgzhang@outlook.com>
2023-01-25 09:43:34 +0800

    update the codes

--- znlp/eval.py
+++ znlp/eval.py
@@ -35,15 +35,7 @@ def evaluate(dataloader,model,loss_fn,device,name,save_file=None):
     y_true = torch.cat(y_true).detach().cpu().numpy()
     all_f1val = 0.0
     all_emval = 0.0
-    if len(y_pred.shape) == 2:
-        _,num_attributes = y_pred.shape
-        for k in range(num_attributes):
-            all_f1val += f1_score(y_true[:,k], y_pred[:,k], average='macro')
-            all_emval += accuracy_score(y_true[:,k],y_pred[:,k])
-        all_f1val /= num_attributes
-        all_emval /= num_attributes
-    else:
-        all_f1val = f1_score(y_true, y_pred, average='macro')
-        all_emval = accuracy_score(y_true,y_pred)
+    all_f1val = f1_score(y_true, y_pred, average='macro')
+    all_emval = accuracy_score(y_true,y_pred)
     all_loss /= len(dataloader)
     return all_loss,all_f1val,all_emval
